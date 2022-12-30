7c23c45e897a32ce25fd5258433545f11ebe95ea
RomainC75 <rom.chenard@gmail.com>
2022-12-30 13:55:20 +0100

    18

--- code/01-realtime/client/src/App.jsx
+++ code/01-realtime/client/src/App.jsx
@@ -12,9 +12,22 @@ function App() {
 
    const handleUsername = (e) => {
       e.preventDefault()
-      console.log(username)
-      socket.emit('username', username)
-      setConnected(true)
+      // console.log(username)
+      // socket.emit('username', username)
+      // setConnected(true)
+
+      // add the property "username"
+      socket.auth = { username }
+      // try to connect
+      socket.connect()
+      console.log(socket)
+
+      setTimeout(() => {
+         if (socket.connected) {
+            console.log('socket.connected', socket)
+            setConnected(true)
+         }
+      }, 300)
    }
 
    useEffect(() => {
@@ -32,13 +45,43 @@ function App() {
    }, [])
 
    useEffect(() => {
-      socket.on('users', (users) => {
-         setUsers(users)
+      socket.on('user connected', (user) => {
+         
+         user.connected = true
+         user.messages = []
+         user.hasNewMessages = false
+         setUsers(previousUsers=>[...previousUsers, user])
+         console.log(users)
       })
+
+      socket.on('users', (users) => {
+         users.forEach((user) => {
+            user.self = user.userID === socket.id
+            user.connected = true
+            user.messages = []
+            user.hasNewMessages = false
+         })
+
+         setUsers(users.sort((a,b)=>b.self))
+      })
+
+      socket.on('user disconnected', id=>{
+         console.log(users)
+         setUsers(previousUsers=>previousUsers.map(user=> {
+            return {
+               ...user,
+               connected : user.userID===id ? false : user.connected      
+            }
+         }))
+      })
+
       return () => {
          socket.off('users')
       }
+      
    }, [socket])
+
+
 
    const handleMessage = (e) => {
       e.preventDefault()
@@ -48,6 +91,15 @@ function App() {
 
    return (
       <div className="App container text-center">
+          <div className="row">
+            <div className="d-flex justify-content-evenly pt-2 pb-1">
+                  {connected && users.map(user=>
+                     <div key={user.userID}>
+                        {user.username} {user.self && "(youself)"}
+                     </div>)}
+            </div>
+         </div>
+
          <div className="row">
             {connected ? (
                <SendMessage
@@ -64,7 +116,7 @@ function App() {
             )}
          </div>
          <div className="row pt-3">
-            <div className="col-md-8">
+            <div className="col">
                {messages.map((message, index) => (
                   <div
                      key={`${index}-${message}`}
@@ -74,14 +126,21 @@ function App() {
                   </div>
                ))}
             </div>
-            <div className="col-md-4">
+            
+            {/* <div className="col-md-4">
                {users.map((user, index) => (
                   <div key={`${index}-${user}`} className="alert alert-primary">
-                     ==>{user}
+                     "{JSON.stringify(user)}"
                   </div>
                ))}
-            </div>
+            </div> */}
+
          </div>
+
+         <div className="row">
+            <p>{JSON.stringify(users)}</p>
+         </div>
+        
       </div>
    )
 }
--- code/01-realtime/server/controllers/chat.js
+++ code/01-realtime/server/controllers/chat.js
@@ -1,4 +1,3 @@
-
 const users = []
 const addUser = (username) => {
    const name = username.trim().toLowerCase()
@@ -12,30 +11,68 @@ const addUser = (username) => {
 const chat = (io) => {
    console.log('live chat ==>', io.opts)
 
-   
+   //middleware
+   io.use((socket, next) => {
+      // get the "username" sent with socket.auth from the front
+
+      const username = socket.handshake.auth.username
+      if (!username) {
+         return next(new Error('invalid username'))
+      }
+      socket.username = username
+      next()
+   })
+
    io.on('connection', (socket) => {
       // console.log('socket io ', socket.id)
-      socket.on('username', (username, next) => {
-         console.log('username : ', username)
-         if (!addUser(username)) {
-            return next('user name not valid')
-         }
-         //to everyone
-         io.emit('users', users)
-         // everyone BUT the sender
-         socket.broadcast.emit('user joined', ` BROADCAST ${username} joined`)
-      })
 
-      socket.on('message', (message) => {
-         console.log('message received : ', message)
-         io.emit('message', message)
-      })
+      // socket.on('username', (username, next) => {
+      //    console.log('username : ', username)
+      //    if (!addUser(username)) {
+      //       return next('user name not valid')
+      //    }
+      //    //to everyone
+      //    io.emit('users', users)
+      //    // everyone BUT the sender
+      //    socket.broadcast.emit('user joined', ` BROADCAST ${username} joined`)
+      // })
+
+      // socket.on('message', (message) => {
+      //    console.log('message received : ', message)
+      //    io.emit('message', message)
+      // })
+
       
-      //disconnect
-      socket.on('disconnect', () => {
-         console.log('user disconnected ! ')
-      })
-   })
-}
+      let users = []
+      for (let [id, socket] of io.of('/').sockets) {
+         const existingUser = users.find(
+            (user) => user.username === socket.username
+            )
+            if (existingUser) {
+               socket.emit('username take')
+               socket.disconnect()
+            } else {
+               users.push({
+                  userID: id,
+                  username: socket.username,
+               })
+            }
+         }
+         // notify the existing users
+         socket.emit('users', users)
+         
+         socket.broadcast.emit('user connected', {
+            userID: socket.id,
+            username: socket.username
+         })
+         
+         //disconnect
+         socket.on('disconnect', () => {
+            socket.broadcast.emit('user disconnected',socket.id)
 
-export default chat
+         })
+      })
+   }
+   
+   export default chat
+   
